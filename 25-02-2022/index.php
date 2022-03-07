e797e6081e8b35aa5554e590186d9bd5068bccb8
jasmin-isapara <jasmin.arsenaltech@gmail.com>
2022-03-07 15:22:56 +0530

    new files

---
+++ 25-02-2022/index.php
@@ -0,0 +1,214 @@
+<html>
+<head>
+    
+    <title>Document</title>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@200&display=swap" rel="stylesheet">
+
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Jasmin Isapara</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="services.php">Services</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="about.php">About Us</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="contact.php">Contact Us</a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav> 
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="7.jpeg" alt="Los Angeles" width="1100" height="500">
+    </div>
+    
+    <div class="carousel-item">
+      <img src="3.jpg" alt="New York" width="1100" height="500">
+    </div>
+    <div class="carousel-item">
+      <img src="4.jpg" alt="New-zeland" width="1100" height="500">
+    </div>
+    <div class="carousel-item">
+      <img src="5.jpg" alt="Canada" width="1100" height="500">
+    </div>
+  </div>
+  
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+<secton class="my-5">
+  <div class="py-5">
+    <h2 class="text-center"><b>About Us</b></h2>
+  </div>
+  <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-6 col-md-6 col-12">
+          <img src="1.jpeg" class="image-fluid about_image">
+        </div>
+        <div class="col-lg-6 col-md-6 col-12">
+          <h2 class="display-4"><b>I AM JASMIN ISAPARA</b></h2>
+          <p class="">
+          My name is jasmin isapara. I am basically from Surat. I have completed my graduation in BCA. and MCA from University. I did my schooling from excellence school Shreee Gyanmanjari Vidhyapith. In my family, we have 6 members including me.My short-term goal is to become a Web_devloper and my long-term goal is to going Out Of Country. My strengths are I am friendly in nature and adapt any circumstances. That's all. Thanks.
+          </p>
+          <a href="about.php" class="btn btn-success" ><b>Check More</b></a>
+        </div>
+      </div>
+  </div>
+</secton>
+
+<secton class="my-5">
+  <div class="py-5">
+    <h2 class="text-center"><b>Our Services</b></h2>
+  </div>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col lg-4 col-md-4 col-12">
+        <div class="card" >
+          <img class="card-img-top" src="8.jpeg" alt="Card image">
+          <div class="card-body">
+          <h4 class="card-title"><b>Dubai</b></h4>
+          <p class="card-text"></p>
+          <a href="#" class="btn btn-primary">See Profile</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col lg-4 col-md-4 col-12">
+        <div class="card" >
+          <img class="card-img-top" src="10.jpeg" alt="Card image">
+          <div class="card-body">
+          <h4 class="card-title"><b>Ladakh</b></h4>
+          <p class="card-text"></p>
+          <a href="#" class="btn btn-primary">See Profile</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col lg-4 col-md-4 col-12">
+        <div class="card" >
+          <img class="card-img-top" src="11.jpeg" alt="Card image">
+          <div class="card-body">
+          <h4 class="card-title"><b>Kerala</b></h4>
+          <p class="card-text"></p>
+          <a href="#" class="btn btn-primary">See Profile</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</section>
+
+
+<secton class="my-5">
+  <div class="py-5">
+    <h2 class="text-center"><b>Our Gallery </b></h2>
+  </div>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-12">
+        <img src="9.jpg" class="img-fluid pb-4">
+      </div>
+      <div class="col-lg-4 col-md-4 col-12">
+        <img src="9.jpg" class="img-fluid pb-4">
+      </div>
+      <div class="col-lg-4 col-md-4 col-12">
+        <img src="9.jpg" class="img-fluid pb-4">
+      </div>
+      <div class="col-lg-4 col-md-4 col-12">
+        <img src="9.jpg" class="img-fluid pb-4">
+      </div><div class="col-lg-4 col-md-4 col-12">
+        <img src="9.jpg" class="img-fluid pb-4">
+      </div><div class="col-lg-4 col-md-4 col-12">
+        <img src="9.jpg" class="img-fluid pb-4">
+      </div>  
+    </div>
+
+  </div>
+</section>
+
+<secton class="my-5">
+  <div class="py-5">
+    <h2 class="text-center"><b>Our Gallery </b></h2>
+  </div>
+
+  <div class="w-50 m-auto">
+    <form action="userinfo.php" method="post">
+      <div class="form-group">
+        <label><b>Username</b></label>
+        <input type="text" name="username" autocomplete="off" class="form-control">
+      </div>
+      <div class="form-group">
+        <label><b>Email-id</b></label>
+        <input type="text" name="email" autocomplete="off" class="form-control">
+      </div>
+      <div class="form-group">
+        <label><b>Mobile</b></label>
+        <input type="text" name="mobile" autocomplete="off" class="form-control">
+      </div>
+      <div class="form-group">
+        <label><b>Comment</b></label>
+        <textarea class="form-control"></textarea>
+      </div>
+      <button type="submit" class="btn btn-success"><b>Submit</b></button>
+    </form>
+  </div>
+</section>
+<footer>
+  <p align="center" class="p-3 bg-dark text-white">@JD Consultancy</p>
+</footer>
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
